29217ba147b40d383c6eee898ff280c992fbddfc
lfilipe09 <81202572+lfilipe09@users.noreply.github.com>
2022-05-08 20:27:38 -0300

    feat: finish components creation

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,67 @@
+import { ArrowLeft } from "phosphor-react-native";
+import React from "react";
+import { 
+  View,
+  TextInput,
+  Image,
+  Text,
+  TouchableOpacity
+} from "react-native";
+import { theme } from "../../theme";
+import { feedbackTypes } from "../../utils/feedbackTypes";
+import { Button } from "../Button";
+import { ScreenshotButton } from "../ScreenshotButton";
+import { FeedbackType } from "../Widget";
+
+import { styles } from "./styles";
+
+
+interface Props {
+  feedbackType: FeedbackType
+}
+
+export function Form({feedbackType}: Props){
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  return(
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <ArrowLeft 
+            size={24}
+            weight={'bold'}
+            color={theme.colors.text_secondary}
+          />
+        </TouchableOpacity>
+
+        <View style={styles.titleContainer}>
+          <Image
+            source={feedbackTypeInfo.image}
+            style={styles.image}
+          />
+          <Text style={styles.titleText}>
+            {feedbackTypeInfo.title}
+          </Text>
+        </View>
+      </View>
+
+      <TextInput
+        multiline
+        style={styles.input}
+        placeholder={"Teve uma ideia de melhoria ou de nova funcionalidade? Conta pra gente!"}
+        placeholderTextColor={theme.colors.text_secondary}
+      />
+
+      <View style={styles.footer}>
+        <ScreenshotButton 
+          onTakeShot={() => {}}
+          OnRemoveShot={() => {}}
+          screenshot={''}
+        />
+
+        <Button isLoading={true} />
+      </View>
+
+    </View>
+  )
+}
--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,12 +1,51 @@
-import React from "react";
-import { View } from 'react-native'
+import React, { useRef } from 'react'
+import { TouchableOpacity } from 'react-native'
+import { ChatTeardropDots } from 'phosphor-react-native'
+import BottomSheet from '@gorhom/bottom-sheet'
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 
 import { styles } from './styles';
+import { theme } from "../../theme";
+import { feedbackTypes } from '../../utils/feedbackTypes'
+import { Form } from '../Form'
 
-export function Widget(){
+export type FeedbackType = keyof typeof feedbackTypes;
+
+function Widget(){
+
+  const bottomSheetRef = useRef<BottomSheet>(null)
+
+  function handleOpen(){
+    bottomSheetRef.current?.expand()
+  }
+
   return(
-    <View style={styles.container}>
+    <>
+    <TouchableOpacity 
+      style={styles.button}
+      onPress={handleOpen}
+    >
+      <ChatTeardropDots 
+        size={24}
+        weight="bold"
+        color={theme.colors.text_on_brand_color}
+      />
+      
+    </TouchableOpacity>
 
-    </View>
+    <BottomSheet 
+      ref={bottomSheetRef}
+      snapPoints={[1, 280]}
+      backgroundStyle={styles.modal}
+      handleIndicatorStyle={styles.indicator}
+    >
+      <Form
+        feedbackType='BUG'
+      />
+    </BottomSheet>
+
+    </>
   )
 }
+
+export default gestureHandlerRootHOC(Widget)
